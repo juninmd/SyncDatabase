30f60e50b45dd42f3209712bceb9c40724109488
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2017-05-16 21:47:04 -0300

    Cadastro de Clientes

---
+++ formCadastro.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace CadastroClientes
+{
+    partial class formCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(526, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cadastrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(227, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(283, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(186, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(56, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(113, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Codigo";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(592, 222);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(18, 301);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(592, 56);
+            this.listBox1.TabIndex = 4;
+            // 
+            // formCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 367);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "formCadastro";
+            this.Text = "Cadastro";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ formCadastro.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CadastroClientes
+{
+    public partial class formCadastro : Form
+    {
+        public formCadastro()
+        {
+            InitializeComponent();
+            MostraResultados();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var query = $"INSERT INTO {ConfigurationManager.AppSettings["schema"]}.cliente VALUES('{textBox2.Text}','{textBox1.Text}')";
+                var guid = Guid.NewGuid();
+
+                Directory.CreateDirectory(ConfigurationManager.AppSettings["caminhoArquivo"]);
+
+                var filename = $"{ConfigurationManager.AppSettings["caminhoArquivo"]}\\{guid}.txt";
+                var bdConn = new MySqlConnection(ConfigurationManager.AppSettings["connectionString"]);
+                bdConn.Open();
+                if (bdConn.State == ConnectionState.Open)
+                {
+                    //Se estiver aberta insere os dados na BD
+                    var commS = new MySqlCommand(query, bdConn);
+                    commS.ExecuteNonQuery();
+                    bdConn.Close();
+                    File.WriteAllText(filename, query.Replace(ConfigurationManager.AppSettings["schema"], "#changeme#"));
+                    MessageBox.Show("Gravado com Sucesso!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MostraResultados();
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Deu Erro!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+        }
+
+        private void MostraResultados()
+        {
+            var mDataSet = new DataSet();
+            var mConn = new MySqlConnection(ConfigurationManager.AppSettings["connectionString"]);
+            mConn.Open();
+
+            //cria um adapter utilizando a instrução SQL para aceder à tabela
+            var mAdapter = new MySqlDataAdapter($"SELECT * FROM {ConfigurationManager.AppSettings["schema"]}.cliente", mConn);
+
+            //preenche o dataset através do adapter
+            mAdapter.Fill(mDataSet, "tabela_dados");
+
+            //atribui o resultado à propriedade DataSource da dataGridView
+            dataGridView1.DataSource = mDataSet;
+            dataGridView1.DataMember = "tabela_dados";
+        }
+    }
+}
